a9887f4f76f168d6adf0eae646515f54d6894bd3
Vlad <vladmdgolam@ya.ru>
2023-12-25 19:05:53 +0100

    Update base URL in Vite config and refactor Three.js mesh creation

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://shapes-vosk-dynamic.vercel.app',
   server: {
     host: 'localhost',
     cors: '*',
@@ -23,11 +24,7 @@ export default defineConfig({
         entryFileNames: 'main.js',
         esModule: false,
         compact: true,
-        globals: {
-          jquery: '$',
-        },
       },
-      external: ['jquery'],
     },
   },
 })
