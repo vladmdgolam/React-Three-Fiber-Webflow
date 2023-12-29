7f0cc40895cf6a026fe4b7dd4c827b9915f5fe23
Vlad <vladmdgolam@ya.ru>
2023-12-29 13:25:33 +0100

    catch errors

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,22 @@ import { createRoot } from 'react-dom/client'
 import App, { videos } from './App'
 
 function initializeReactApp() {
-  createRoot(document.getElementById('root-og')).render(
-    <App video={videos.og} />
-  )
-  createRoot(document.getElementById('root-lines')).render(
-    <App video={videos.lines} />
-  )
-  createRoot(document.getElementById('root-spheres')).render(
-    <App video={videos.spheres} />
-  )
-  createRoot(document.getElementById('root-cubes')).render(
-    <App video={videos.cubes} />
-  )
+  try {
+    createRoot(document.getElementById('root-og')).render(
+      <App video={videos.og} />
+    )
+    createRoot(document.getElementById('root-lines')).render(
+      <App video={videos.lines} />
+    )
+    createRoot(document.getElementById('root-spheres')).render(
+      <App video={videos.spheres} />
+    )
+    createRoot(document.getElementById('root-cubes')).render(
+      <App video={videos.cubes} />
+    )
+  } catch (error) {
+    console.error('Failed to initialize React app:', error)
+  }
 }
 
 if (document.readyState === 'loading') {
